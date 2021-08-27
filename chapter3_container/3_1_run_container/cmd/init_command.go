1e99abc43ccd9d0c2440acbab06c3ec0d3fb151b
JasonkayZK <271226192@qq.com>
2021-08-27 16:46:50 +0000

    restructure 3.1

---
+++ chapter3_container/3_1_run_container/cmd/init_command.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+	"my_docker/container"
+)
+
+var InitCommand = cli.Command{
+	Name:  "init",
+	Usage: "Init container process run user's process in container. Do not call it outside",
+	Action: func(context *cli.Context) error {
+		log.Infof("init come on")
+		cmd := context.Args().Get(0)
+		log.Infof("command %s", cmd)
+		err := container.RunContainerInitProcess(cmd, nil)
+		return err
+	},
+}
--- chapter3_container/3_1_run_container/cmd/run_command.go
+++ chapter3_container/3_1_run_container/cmd/run_command.go
@@ -34,17 +34,7 @@ var RunCommand = cli.Command{
 	},
 }
 
-var InitCommand = cli.Command{
-	Name:  "init",
-	Usage: "Init container process run user's process in container. Do not call it outside",
-	Action: func(context *cli.Context) error {
-		log.Infof("init come on")
-		cmd := context.Args().Get(0)
-		log.Infof("command %s", cmd)
-		err := container.RunContainerInitProcess(cmd, nil)
-		return err
-	},
-}
+
 
 func run(tty bool, command string) {
 	parent := container.NewParentProcess(tty, command)

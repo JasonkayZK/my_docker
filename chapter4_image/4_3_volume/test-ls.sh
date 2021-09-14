12f223c15bdc02a8e710b69dfeef057e26a8bdfd
JasonkayZK <271226192@qq.com>
2021-09-14 21:35:00 +0000

    init 4-3

---
+++ chapter4_image/4_3_volume/test-ls.sh
@@ -0,0 +1,13 @@
+############################## Test 0：数据准备 ##############################
+# 解压缩：将 `busybox.tar` 放在 `/root/my-docker` 目录
+pwd
+# /root/my-docker
+
+ll
+# total 1444
+# drwxr-xr-x  3 root root    4096 Sep 14 20:43 ./
+# drwx------ 13 root root    4096 Sep 14 20:54 ../
+# drwxr-xr-x 12 root root    4096 Sep 14 15:03 busybox/
+# -rw-------  1 root root 1464320 Sep 14 12:30 busybox.tar
+
+
